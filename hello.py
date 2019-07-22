804a62f205671c6b8ab572a0b9dc678c5eb7bc29
mariana fernandez garcia <marianafernandezgarcia@gmail.com>
2019-07-22 10:56:56 +0200

    clase 13, primera parte de estructura completa

--- hello.py
+++ hello.py
@@ -1,28 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return """
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Mi primera página</title>
-        </head>
-        <body>
-            <h1>Título de mi página</h1>
-            <p>Hola, mundo</p>
-        </body>
-        </html>    
-    
-    """
-
+def hello_world():
+    return "Hola, mundo"
 
 @app.route('/adios')
 def bye():
-    return """
-    {"success":false,"error":{"code":101,"type":"missing_access_key","info":"You have not supplied an API Access Key. [Required format: access_key=YOUR_ACCESS_KEY]"}}
-    """
+    return 'Adios, mundo cruel'
